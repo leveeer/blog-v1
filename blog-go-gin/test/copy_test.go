4fd60f6b8e15b6bb53880885a2851d20b1d5feab
Einson <1519695805@qq.com>
2021-06-16 19:45:35 +0800

    add test

---
+++ blog-go-gin/test/copy_test.go
@@ -0,0 +1,53 @@
+package test
+
+import (
+	"blog-go-gin/common"
+	"blog-go-gin/config"
+	"blog-go-gin/dao"
+	pb "blog-go-gin/go_proto"
+	"blog-go-gin/logging"
+	"testing"
+	"time"
+)
+
+func init() {
+	config.GetConf()
+	dao.InitMysql()
+	common.InitRedis()
+}
+
+func TestCopyField(t *testing.T) {
+	a1 := &pb.Article{
+		Id:             30,
+		UserId:         1,
+		CategoryID:     19,
+		ArticleCover:   "ecfbrehbvguherf",
+		ArticleTitle:   "测试",
+		ArticleContent: "测试内容",
+		CreateTime:     time.Now().Unix(),
+		UpdateTime:     time.Now().Unix(),
+		IsTop:          true,
+		IsPublish:      true,
+		IsDelete:       false,
+		IsOriginal:     true,
+		ClickCount:     9999,
+		CollectCount:   8888,
+		CategoryName:   "测试分类",
+		Tags: []*pb.Tag{
+			{
+				TagName: "测试tag1",
+			}, {
+				TagName: "测试tag2",
+			},
+		},
+	}
+	a2 := &pb.Article{}
+
+	err := common.Copy(&a2, a1).Do()
+	if err != nil {
+		return
+	}
+
+	logging.Logger.Info(a2)
+
+}
