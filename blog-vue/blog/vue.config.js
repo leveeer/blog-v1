0ceb781c258c781fc6ef89a06b5cb09ad0b32d4d
Einson <1519695805@qq.com>
2021-07-05 17:33:46 +0800

    fix vue config

--- blog-vue/blog/vue.config.js
+++ blog-vue/blog/vue.config.js
@@ -14,7 +14,6 @@ module.exports = {
       // proxy all requests starting with /api to jsonplaceholder
       "/api": {
         target: "http://localhost:8088",
-        // target: "http://47.119.168.227:8088",
         changeOrigin: true,
         ws: true,
         pathRewrite: {
